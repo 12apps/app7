fe6ec4fa9407488af76b59adda7e662cbf4e375a
Richard Tanner <dicktanner@gmail.com>
2014-12-18 15:58:48 -0800

    Add underscore/backbone to view

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,30 @@
 (function(){
-  $.ajax({
-    url: "/yelp" + "?latlon=fremont"
-  }).done(function(data){
-    console.log(data);
-  })
+  var businesses ={};
+
+  refresh();
+
+  ResView = Backbone.View.extend({
+    template: _.template( $("#restaurants").html() ),
+    render: function(data){
+      // Compile the template using underscore
+      this.$el.html( this.template(data) );
+    }
+  });
+
+  function update(){
+    var restaurants_view = new ResView({ el: $("#container") });
+    restaurants_view.render({ 'businesses': businesses});
+    // id of the div you want to append the template result to
+  }
+
+
+  function refresh (){
+    $.ajax({
+      url: "/yelp" + "?latlon=fremont"
+    }).done(function(data){
+      businesses = data.businesses;
+      update();
+      console.log(data);
+    })
+  }
 })();
